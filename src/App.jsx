3e4693f8f876fcb9e37ef8cdb9f8a94f6a400116
tarikjaber <tariknjaber@gmail.com>
2024-02-09 10:30:42 -0500

    Make time based on date not interval

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
+import { createEffect, createSignal, onCleanup } from 'solid-js'
 import './App.css'
 
 function App() {
-  const [count, setCount] = createSignal(0)
+  const TEN_MINUTES = 10 * 60
+  const FIVE_MINUTES = 5 * 60
+  const ONE_MINUTE = 1 * 60
+  const startTime = new Date()
+  const [time, setTime] = createSignal(TEN_MINUTES)
+  
+  createEffect(() => {
+    if (time() <= 0) {
+      setTime(TEN_MINUTES)
+      new Notification("Interval over!")
+    } else if (time() === FIVE_MINUTES) {
+      new Notification("Halfway done!")
+    } else if (time() === ONE_MINUTE) {
+      new Notification("One minute left!")
+    }
+  })
+  
+  const timer = setInterval(() => {
+    let endTime = Math.floor(+startTime / 1000) + TEN_MINUTES
+    let currTime = Math.floor(+(new Date()) / 1000)
+    let secondsLeft = endTime - currTime
+    setTime(secondsLeft)
+  }, 1000)
+  onCleanup(() => clearInterval(timer))
+  
+  function formatTime(time) {
+    let minutes = Math.floor(time / 60)
+    let minuteString = String(minutes).padStart(2, '0')
+    let seconds = time % 60
+    let secondString = String(seconds).padStart(2, '0')
 
+    return minuteString + ":" + secondString
+  }
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
-    </>
+    <h1>{formatTime(time())}</h1>
   )
 }
 
