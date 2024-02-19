544221871ab06906548e118c2e75dc2b717071df
tarikjaber <tariknjaber@gmail.com>
2024-02-19 11:59:44 -0500

    Revert "attempt"
    This reverts commit d962aa78bc5ebabd9bf9e38dbefb2e055f90f13e.

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,31 @@ import { createEffect, createSignal, onCleanup } from 'solid-js'
 import './App.css'
 
 function App() {
+  const TEN_MINUTES = 10 * 60
   const FIVE_MINUTES = 5 * 60
   const ONE_MINUTE = 1 * 60
-  const [time, setTime] = createSignal(100)
+  const [time, setTime] = createSignal(TEN_MINUTES)
   const [startTime, setStartTime] = createSignal(new Date());
   
   createEffect(() => {
-    const timer = setInterval(() => {
-      let endTime = Math.floor(+startTime() / 1000) + time()
-      let currTime = Math.floor(+(new Date()) / 1000)
-      let secondsLeft = endTime - currTime
-
-      setTime(secondsLeft)
-
-      if (time() <= 0) {
-        setTime(100)
-        setStartTime(new Date())
-        new Notification("Interval over!")
-      } else if (time() === FIVE_MINUTES) {
-        new Notification("Halfway done!")
-      } else if (time() === ONE_MINUTE) {
-        new Notification("One minute left!")
-      }
-    }, 1000)
-    onCleanup(() => clearInterval(timer))
+    if (time() <= 0) {
+      setTime(TEN_MINUTES)
+      setStartTime(new Date())
+      new Notification("Interval over!")
+    } else if (time() === FIVE_MINUTES) {
+      new Notification("Halfway done!")
+    } else if (time() === ONE_MINUTE) {
+      new Notification("One minute left!")
+    }
   })
+  
+  const timer = setInterval(() => {
+    let endTime = Math.floor(+startTime() / 1000) + TEN_MINUTES
+    let currTime = Math.floor(+(new Date()) / 1000)
+    let secondsLeft = endTime - currTime
+    setTime(secondsLeft)
+  }, 1000)
+  onCleanup(() => clearInterval(timer))
   
   function formatTime(time) {
     let minutes = Math.floor(time / 60)
@@ -38,10 +38,7 @@ function App() {
   }
   
   return (
-    <>
-      <h1>{time()}</h1>
-      <h1>{formatTime(time())}</h1>
-    </>
+    <h1>{formatTime(time())}</h1>
   )
 }
 
