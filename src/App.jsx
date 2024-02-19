6ea65455a8132d8cc624c51e0c77963fd250edf1
tarikjaber <tariknjaber@gmail.com>
2024-02-19 13:22:12 -0500

    rand n

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,23 @@ import { createEffect, createSignal, onCleanup } from 'solid-js'
 import './App.css'
 
 function App() {
-  const TEN_MINUTES = 10 * 60
   const FIVE_MINUTES = 5 * 60
   const ONE_MINUTE = 1 * 60
-  const [time, setTime] = createSignal(TEN_MINUTES)
+  const randomTime = () => Math.floor(Math.random() * 30 * 60 + 1)
+  let timeLength = randomTime()
+  const [time, setTime] = createSignal(timeLength)
   const [startTime, setStartTime] = createSignal(new Date());
+  
+  function intervalOver() {
+    timeLength = randomTime()
+    setTime(timeLength)
+    setStartTime(new Date())
+  }
   
   createEffect(() => {
     if (time() <= 0) {
-      setTime(TEN_MINUTES)
-      setStartTime(new Date())
-      new Notification("Interval over!")
+      intervalOver()
+      new Notification("Time out!")
     } else if (time() === FIVE_MINUTES) {
       new Notification("Halfway done!")
     } else if (time() === ONE_MINUTE) {
@@ -21,7 +27,8 @@ function App() {
   })
   
   const timer = setInterval(() => {
-    let endTime = Math.floor(+startTime() / 1000) + TEN_MINUTES
+    console.log(timeLength)
+    let endTime = Math.floor(+startTime() / 1000) + timeLength
     let currTime = Math.floor(+(new Date()) / 1000)
     let secondsLeft = endTime - currTime
     setTime(secondsLeft)
@@ -37,8 +44,15 @@ function App() {
     return minuteString + ":" + secondString
   }
   
+  function done() {
+    intervalOver()
+  }
+  
   return (
-    <h1>{formatTime(time())}</h1>
+    <>
+      <h1>{formatTime(time())}</h1>
+      <button onClick={done}>Done</button>
+    </>
   )
 }
 
